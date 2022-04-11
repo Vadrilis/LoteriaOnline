a85904f2d53c27b21b93d1d447d6a09a2bd6b504
lAlissial <alissialima@hotmail.com>
2022-04-11 20:59:10 -0300

    Initial commit

---
+++ avcobloteriaonline/src/main/java/br/com/edu/ifpb/tsi/pwebii/projeto/loteriaonline/avcobloteriaonline/AvcobloteriaonlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.edu.ifpb.tsi.pwebii.projeto.loteriaonline.avcobloteriaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvcobloteriaonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvcobloteriaonlineApplication.class, args);
+	}
+
+}
---
+++ avcobloteriaonline/src/test/java/br/com/edu/ifpb/tsi/pwebii/projeto/loteriaonline/avcobloteriaonline/AvcobloteriaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edu.ifpb.tsi.pwebii.projeto.loteriaonline.avcobloteriaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvcobloteriaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
